c38c46bad1b1ac9fc0aeef46f96c38d4aac8fc36
Roger <logroger@gmail.com>
2021-04-16 20:23:24 -0300

    first

---
+++ src/main/java/io/github/rogerlog/springroger/SpringRogerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog.springroger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRogerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRogerApplication.class, args);
+	}
+
+}
